0976efed244cd52a75a792db997f5a4dff522afb
Shalini Aradhya <shalinigaradhya@gmail.com>
2026-01-07 14:42:25 +0530

    Added new change

--- web/next.config.js
+++ web/next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '' : ''
+  }
 }
 
 module.exports = nextConfig
